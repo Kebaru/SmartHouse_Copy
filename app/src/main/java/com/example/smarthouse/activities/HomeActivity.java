199540b8d79685c0c4a1b08c61c68dca1ad2b83e
Kebaru <kebaru@yandex.ru>
2023-01-24 12:05:02 +0900

    2

--- app/src/main/java/com/example/smarthouse/activities/HomeActivity.java
+++ app/src/main/java/com/example/smarthouse/activities/HomeActivity.java
@@ -3,20 +3,58 @@ package com.example.smarthouse.activities;
 import static androidx.navigation.Navigation.findNavController;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import com.example.smarthouse.R;
+import com.example.smarthouse.databinding.ActivityHomeBinding;
+import com.example.smarthouse.fragments.HomeFragment;
+import com.example.smarthouse.fragments.RoutinesFragment;
+import com.example.smarthouse.fragments.SettingFragment;
+import com.example.smarthouse.fragments.StaticsFragment;
 
 public class HomeActivity extends AppCompatActivity {
+
+    ActivityHomeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
 
+        binding.botnav.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.statics:
+                    replaceFragment(new StaticsFragment());
+                    break;
+                case R.id.routines:
+                    replaceFragment(new RoutinesFragment());
+                    break;
+                case R.id.setting:
+                    replaceFragment(new SettingFragment());
+                    break;
+            }
+
+            return true;
+        });
+
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.framelayout, fragment);
+        fragmentTransaction.commit();
     }
 
     public void toKitchen(View v){
